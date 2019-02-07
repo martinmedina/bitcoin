7732d114def3f84110a1d728ba98be7704d4507b
Martín Medina <tinnmedina@gmail.com>
2019-02-07 01:29:00 -0300

    Change halving value to 7500 in regtest

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -263,7 +263,7 @@ class CRegTestParams : public CChainParams {
 public:
     CRegTestParams() {
         strNetworkID = "regtest";
-        consensus.nSubsidyHalvingInterval = 150;
+        consensus.nSubsidyHalvingInterval = 7500;
         consensus.BIP34Height = 100000000; // BIP34 has not activated on regtest (far in the future so block v1 are not rejected in tests)
         consensus.BIP34Hash = uint256();
         consensus.BIP65Height = 1351; // BIP65 activated on regtest (Used in rpc activation tests)
